1a3c60cca33ce95f6b4c4c8405a4c21e2e5cf021
Nikita <62235114+SosiS4ka@users.noreply.github.com>
2020-03-18 00:16:17 +0200

    3
    Started Player Screen

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public string QuestionText;
+
+    void Start()
+    {
+   
+    }
+
+    void Update()
+    {
+                
+    }
+}
